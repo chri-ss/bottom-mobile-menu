4b323b597212b44b3420c9557355dceaa9e72282
Chris Cresswell <christorper@gmail.com>
2022-01-21 22:17:56 -0800

    Resize view height when screen size changes

--- src/viewHeight.js
+++ src/viewHeight.js
@@ -1,6 +1,10 @@
 const calcViewHeight = () => {
   let vh = window.innerHeight * 0.01;
   document.documentElement.style.setProperty("--vh", `${vh}px`);
+  window.addEventListener('resize', () => {
+      let vh = window.innerHeight * 0.01;
+      document.documentElement.style.setProperty("--vh", `${vh}px`);
+  })
 };
 
 export default calcViewHeight;

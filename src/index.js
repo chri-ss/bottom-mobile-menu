b4e1f71d4e07ef7085f5b7eebdd5005664c5ed4a
Chris Cresswell <christorper@gmail.com>
2022-01-21 22:08:19 -0800

    Fix: mobile screen overflow due to address bar

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import "./style.css";
 import "./reset.css";
-import makeMenu from './menu'
+import makeMenu from "./menu";
+import calcViewHeight from "./viewHeight";
 
+calcViewHeight();
 makeMenu();
